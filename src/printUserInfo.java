d48bff24d0359947c17b086bf3a001e03ade23b1
chopi <chopinguy@cox.net>
2020-11-25 10:43:35 -0700

    Initial Commit intelliJ

---
+++ src/printUserInfo.java
@@ -0,0 +1,11 @@
+//package org.launchcode;
+import java.util.Scanner;
+public class printUserInfo {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give an Integer: ");
+        int input = scan.nextInt();
+        System.out.println("The user inputted a " + input);
+
+    }
+}
